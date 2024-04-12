3a6ec1cf289c97ad5c40065d79469e0f612a9bc8
sundar shrestha <71543785+sundar112@users.noreply.github.com>
2024-04-12 17:11:00 +0530

    till 05

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score) // string to number
+console.log(typeof valueInNumber)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+//1 => true 0=> false
+//"" => false "anystring" => true
+
+
+/******************** Operations ********************* */
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+
---
+++ 01_basics/05_dataTypes_summary.js
@@ -0,0 +1,26 @@
+/******************primitive*********/
+
+// 7 types : String, Number, Boolean, null, undefine, Symbol, BigInt
+
+const score = 100
+
+
+
+
+
+/*******non primitive**** */
+//Array, Objects, Functions
+
+const heros = ["spiderman", "ironman", "thor"];
+
+let myObj = {
+    name: "sundar",
+    age: 27
+}
+
+const myFunction = function(){
+    console.log("hello wored this is function");
+}
+
+
+
